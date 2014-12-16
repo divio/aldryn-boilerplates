a0f8c2ea4005e969957f515180baa939df809853
Stefan Foulis <stefan@foulis.ch>
2014-12-16 10:42:49 +0100

    metadata

--- aldryn_boilerplate/__init__.py
+++ aldryn_boilerplate/__init__.py
@@ -1,2 +1,15 @@
 # -*- coding: utf-8 -*-
-__version__ = '0.1'
+from __future__ import unicode_literals, absolute_import
+
+__version__ = '0.2'
+__author__ = 'Stefan Foulis'
+__license__ = 'BSD'
+__copyright__ = "Copyright 2014, Divio Aldryn Ltd"
+__maintainer__ = 'Stefan Foulis'
+__email__ = 'stefan@foulis.ch'
+__url__ = 'https://github.com/aldryn/aldryn-boilerplate/'
+__status__ = 'Development'
+__doc__ = 'An extension that allows re-usable apps to provide sets of templates and staticfiles for different boilerplates.'  # noqa
+
+
+default_app_config = 'aldryn_boilerplate.apps.AldrynBoilerplateConfig'
---
+++ aldryn_boilerplate/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, absolute_import
+from django.apps import AppConfig
+
+
+class AldrynBoilerplateConfig(AppConfig):
+    name = 'aldryn_boilerplate'
+    verbose_name = "Aldryn Boilerplate"
